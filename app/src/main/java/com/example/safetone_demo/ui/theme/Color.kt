c78eb430baded8b440b73f41bb00f08a2afeee8f
Craciun <craciuncristinaarina@gmail.com>
2026-04-25 14:22:49 +0300

    Interfata Light Mode/ Dark Mode

--- app/src/main/java/com/example/safetone_demo/ui/theme/Color.kt
+++ app/src/main/java/com/example/safetone_demo/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.safetone_demo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Mode
+val LightBackground = Color(0xFFF8F9FA)
+val LightSurface = Color(0xFFFFFFFF)
+val LightPrimary = Color(0xFF2E7D32) // Verdele de siguranță
+val LightError = Color(0xFFD32F2F)   // Roșul de alertă
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Mode
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkPrimary = Color(0xFF81C784)
+val DarkError = Color(0xFFE57373)
